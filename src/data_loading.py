361c0356f2c8a5d9a2e83e8e5a3415e08b4023f6
Tilman Kerl <tilmankerl@protonmail.com>
2023-07-29 18:39:06 +0200

    prep load_swipe_item

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,11 +1,25 @@
 import pandas as pd
 
 
-def load_swipe_items(userMail="bwoollam1d@nytimes.com"):
+def load_swipe_items():
     # exclude userMail from swipe_items
     df = pd.read_csv("data/MOCK_DATA.csv")
+    methods_df = pd.read_csv("data/MOCK_methods.csv")
+    research_interest_df = pd.read_csv("data/MOCK_DATA_topic.csv")
 
-    df = df[df["userMail"] != userMail].head(5)
+    methods_df["swipe_type"] = "method"
+    research_interest_df["swipe_type"] = "topic"
+
+    methods_dict = methods_df.to_dict(orient="records")
+    research_interes_dict = research_interest_df.to_dict(orient="records")
+
+    items = [
+        {"swipe_type": "researchInterestIntro"},
+        research_interes_dict,
+        {"swipe_type": "methodIntro"},
+        methods_dict
+    ]
+    
     df["swipe_type"] = ["researchInterestIntro", "topic", "topic", "methodIntro", "method"]
 
     print(df)
