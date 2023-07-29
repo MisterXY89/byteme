4a5ac9bd306484ae3363ee0e4b65c0196d77c106
ant-le <lechuga.anton@gmail.com>
2023-07-29 17:07:36 +0200

    FIX: Error creating dummy

--- src/generate_dummyDF.py
+++ src/generate_dummyDF.py
@@ -8,12 +8,12 @@ PATH = Path(__file__).parent.parent.joinpath("data")
 
 def generate(n: int, research_size: int, method_size: int):
     weights = np.random.random((n, 2))
-    research = np.random.randint(0, 1, (n, research_size))
-    methods = np.random.randint(0, 1, (n, method_size))
+    research = np.random.randint(0, 2, (n, research_size))
+    methods = np.random.randint(0, 2, (n, method_size))
 
     data = np.hstack((weights, research, methods))
     df = pd.DataFrame(data)
-    df.to_csv(PATH / "dummy.csv")
+    df.to_csv(PATH / "dummy.csv", index=True)
 
 
 if __name__ == "__main__":
